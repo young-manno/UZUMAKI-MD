dcee83903a7b1b6133abe708a741004ceb691fa2
ANONYMOUS UZUMAKI <174778007+young-manno@users.noreply.github.com>
2024-11-29 03:53:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "T6wyhIqC#qmtNeDleNVt-TaA0PP_4guiagxB71EgNKXAkq5Lg_w8", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
